bcd42df2f1e4eac278c35180d5acf3f0922f7e87
Nickolas Moura <nickolas.moura@outlook.com>
2024-06-15 22:39:59 -0300

    CSS Project finished

---
+++ JavaScript/aula-06/index.js
@@ -0,0 +1,17 @@
+
+//Utilize sempre o Camel case no JavaScript!
+var nomeDoAluno = 'Nickolas Moura'; //string
+var idade = 27; //number
+var altura = 1.77; //number
+var estudando = true // boleano, verdadeiro ou falso
+
+console.log(nomeDoAluno, typeof nomeDoAluno)
+console.log(idade, typeof idade)
+console.log(altura, typeof altura)
+console.log(estudando, typeof estudando)
+
+var semConteudo; //declarando uma variável
+
+console.log(semConteudo)
+
+var curso = "front-end em React", topico = "JavaScript Básico 1";
